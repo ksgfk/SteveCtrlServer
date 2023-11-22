c14e192af5502d3af41db4762852a4d6aa71273e
xuesen.xiao <37179748+ksgfk@users.noreply.github.com>
2023-11-22 18:46:09 +0800

    logout

--- backend/SteveCtrl/Controllers/AccountController.cs
+++ backend/SteveCtrl/Controllers/AccountController.cs
@@ -8,14 +8,15 @@ using SteveCtrl.Repositories;
 
 namespace SteveCtrl.Controllers
 {
+    //ApiController特性用于提供 HTTP API 响应, https://learn.microsoft.com/zh-cn/aspnet/core/web-api/?view=aspnetcore-8.0#apicontroller-attribute
     [Route("[controller]/[action]")]
-    [ApiController] //ApiController特性用于提供 HTTP API 响应, https://learn.microsoft.com/zh-cn/aspnet/core/web-api/?view=aspnetcore-8.0#apicontroller-attribute
+    [ApiController, Authorize]
     public class AccountController(ILogger<AccountController> logger, IUserRepository userRepo) : ControllerBase
     {
         private readonly ILogger<AccountController> _logger = logger;
         private readonly IUserRepository _userRepo = userRepo;
 
-        [HttpPost]
+        [HttpPost, AllowAnonymous]
         public async Task<IActionResult> Login(LoginModel model)
         {
             UserModel? user = _userRepo.GetUserByUsernameAndPassword(model.Username, model.Password);
@@ -39,26 +40,23 @@ namespace SteveCtrl.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public IActionResult GetUserInfo()
         {
             var strId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var name = User.FindFirstValue(ClaimTypes.Name);
-            var strRole = User.FindFirstValue(ClaimTypes.Role);
-            if (strId == null ||
-            name == null ||
-            strRole == null ||
-            !int.TryParse(strId, out int id) ||
-            !Enum.TryParse(strRole, out UserRole role))
-            {
-                return BadRequest();
-            }
+            int id = int.Parse(strId ?? string.Empty);
             UserModel? user = _userRepo.GetUserById(id);
             if (user == null)
             {
                 return BadRequest();
             }
             return Ok(new { user.Id, user.Name, user.Role });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Ok();
         }
     }
 }
